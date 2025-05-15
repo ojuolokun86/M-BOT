bc950e634da5905fbd46a2366a2cded18e1b129c
ojuolokun86 <ojuolokun86@gmail.com>
2025-05-15 17:53:47 +0100

    Describe server

--- src/server/server.js
+++ src/server/server.js
@@ -90,10 +90,11 @@ const createServer = () => {
     });
 
     // Start the server
-    const PORT = process.env.PORT;
-   server.listen(PORT, '0.0.0.0', () => {
-    console.log(`✅ Server is running on http://0.0.0.0:${PORT}`);
-});
+   const PORT = process.env.PORT || 3000;
+    server.listen(PORT, '0.0.0.0', () => {
+    console.log(`✅ Server running on port ${PORT}`);
+    });
+
 
 };
 
